de368500484c49994eaa9a92ce02b7b4e29e2e1b
Lacunzo <50136299+Lacunzo@users.noreply.github.com>
2019-05-07 15:55:37 +0200

    chouch

--- Foot.java
+++ Foot.java
@@ -10,8 +10,8 @@ public class Foot {
     
     public static void main(String[] args) {
 	    int l=0 ;
-	    //int connexion= BD.ouvrirConnexion("172.20.128.64","claudel_BD","claudel","claudel");
-	    int connexion = BD.ouvrirConnexion("localhost", "Championnat", "root", "");
+	    int connexion= BD.ouvrirConnexion("172.20.128.64","claudel_BD","claudel","claudel");
+	    //int connexion = BD.ouvrirConnexion("localhost", "Championnat", "root", "");
 	    String log = "";
 	    String mdp = "";
 	    
@@ -46,6 +46,19 @@ public class Foot {
 				Ecran.afficher("Voulez vous modifier les journées / résultat ( o : oui , n : non ) : \n");
 				modif =  Clavier.saisirChar();
 				if (modif != 'n'){
+					Ecran.afficher("ID : \n");
+					String maID =  Clavier.saisirString();
+					Ecran.afficher("equipe domicile : \n");
+					String eqDomi =  Clavier.saisirString();
+					Ecran.afficher("equipe ext  : \n");
+					String eqExt =  Clavier.saisirString();
+					Ecran.afficher("score domi : \n");
+					String scDomi =  Clavier.saisirString();
+					Ecran.afficher("score ext : \n");
+					String scExt =  Clavier.saisirString();
+					if(BD.executerSelect(connexion, "SELECT * FROM `match`WHERE maID = '"+maID+"' ")>=0)
+						BD.executerUpdate(connexion,"INSERT INTO `claudel_BD`.`match` (`maID`, `maEquipe1`, `maEquipe2`, `maScoreEquipe1`, `maScoreEquipe2`) VALUES ('"+maID+"', '"+eqDomi+"', '"+eqExt+"', '"+scDomi+"', '"+scExt+"');");
+					
 					
 					
 				}
